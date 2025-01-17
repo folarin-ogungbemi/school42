163e3daba78002b1b9392f9ce5724441a6781a6d
Folarin Ogungbemi <foogungb@c3a1c6.42berlin.de>
2025-01-17 21:39:45 +0100

    Incomplete: Add bomus files

---
+++ GetNextLine/get_next_line_bonus.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: foogungb <foogungb@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/17 21:34:53 by foogungb          #+#    #+#             */
+/*   Updated: 2025/01/17 21:34:55 by foogungb         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+char	*ft_strdup(const char *s);
+size_t	ft_strlen(const char *str);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
