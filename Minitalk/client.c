b3b48d24d9157b8de74ec7a216690665f40dbe5f
folarin-ogungbemi <ftogungbemi@gmai.com>
2025-06-27 10:25:00 +0200

    validate user input

--- Minitalk/client.c
+++ Minitalk/client.c
@@ -1,5 +1,27 @@
 #include "minitalk.h"
 
+int	ft_isdigit(char c)
+{
+	if (c >= 48 && c <= 57)
+		return (1);
+	return (0);
+}
+
+int	ft_atoi_strict(const char *nptr)
+{
+	int	nbr;
+
+	nbr = 0;
+	while (*nptr)
+	{
+		if (!ft_isdigit(*nptr))
+			return (-1);
+		nbr = nbr * 10 + (*nptr - 48);
+		nptr++;
+	}
+	return (nbr);
+}
+
 void	send_char(pid_t client_pid, char c)
 {
 	int	i;
@@ -25,7 +47,12 @@ int	main(int ac, char **av)
 		write(2, "Usage: ./client <server-id>\n", 29);
 		return (1);
 	}
-	pid = atoi(av[1]);
+	pid = ft_atoi_strict(av[1]);
+	if (pid <= 0 || kill(pid, 0) == -1)
+	{
+		write(2, "Invalid PID\n", 12);
+		return (1);
+	}
 	while (*av[2])
 		send_char(pid, *av[2]++);
 	send_char(pid, '\0');
